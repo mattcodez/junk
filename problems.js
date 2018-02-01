a0ef65421dfa9716a4ee498f5df4321276efb6bb
Matthew Molnar <matthew.molnar@gmail.com>
2018-02-01 08:33:29 -0500

    Create problems.js

---
+++ problems.js
@@ -0,0 +1,14 @@
+// reverse a string recursively
+
+{
+  const str = "this is my string";
+
+  // recursively
+
+  function reverse(str) {
+    if (str.length === 0) return "";
+    return str[str.length - 1] + reverse(str.substr(0, str.length - 1));
+  }
+
+  console.log(reverse(str));
+}
