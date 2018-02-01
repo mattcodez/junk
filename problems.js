12ddbc747c3e26bb3a5a09d69ea28a0852f1e0c3
Matthew Molnar <matthew.molnar@gmail.com>
2018-02-01 08:53:42 -0500

    Update problems.js

--- problems.js
+++ problems.js
@@ -10,3 +10,16 @@
 
   console.log(reverse(str));
 }
+
+
+// get fib numbers
+{
+  function fib(prev1, prev2, nth) {
+    if (nth <= 0) return [];
+    const next = prev1 + prev2;
+    if (next === 0) return [1, ...fib(1, 0, --nth)];
+    return [next, ...fib(next, prev1, --nth)];
+  }
+
+  console.log(fib(0, 0, 10))
+}
